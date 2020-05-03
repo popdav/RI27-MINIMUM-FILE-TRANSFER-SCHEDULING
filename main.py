1ae404cca75278cf82cf81dc17957a4fa2cac5c4
StefanGolubovic <stefan.golubovic3@gmail.com>
2020-05-03 14:52:11 +0200

    Graph prot

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from MockNetwork import Network
-
+from matplotlib import pyplot as plt
+import numpy as np
+import networkx as nx
+from itertools import product
 
 def main():
     n = Network()
     n.init_network()
 
+    G = nx.Graph()
+
     for server in n:
-        print(server)
+        G.add_node(server.get_id(), pos = (server.longitude, server.latitude))
+
+    G.add_edges_from((a, b) for a, b in product(range(10), range(10)) if a != b)
+
+    nx.draw(G, nx.get_node_attributes(G, 'pos'), with_labels=True, node_size=400)
+    plt.show()
 
 
 if __name__ == '__main__':
